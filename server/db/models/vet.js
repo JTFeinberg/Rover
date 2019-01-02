e5377dfe8e1600590e2c27c686a41cd09ad71dc1
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-02 14:16:53 -0500

    feat(vet): Vets should have email, name, location, image, and description

--- server/db/models/vet.js
+++ server/db/models/vet.js
@@ -1,6 +1,29 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Vet = db.define('vet', {})
+const Vet = db.define('vet', {
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
+  location: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  image: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT
+  }
+})
 
 module.exports = Vet
