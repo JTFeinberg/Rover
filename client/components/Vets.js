567269643007259fbd7960b4f458f76daeaef773
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-02 15:13:34 -0500

    chore(Vets): Do all imports now

--- client/components/Vets.js
+++ client/components/Vets.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {fetchVetsThunk} from '../store'
 
 export default class Vets extends Component {
   render() {
