45c0c78b2cc7d3bf6af05dbde7ab8f787758b0b1
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-02 15:55:20 -0500

    feat(routes): Add route to a specific vet

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Vets} from './components'
+import {Login, Signup, UserHome, Vets, SingleVet} from './components'
 import {me} from './store'
 
 /**
@@ -22,6 +22,7 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/vets" component={Vets} />
+        <Route path="/vets/:vetId" component={SingleVet} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
